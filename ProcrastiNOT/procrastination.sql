539baf8071a9918bb674aa7f1af8b3a81d6e844c
ginandjustice <42471225+ginandjustice@users.noreply.github.com>
2018-12-14 22:24:46 -0500

    Add files via upload

--- ProcrastiNOT/procrastination.sql
+++ ProcrastiNOT/procrastination.sql
@@ -2,10 +2,10 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 12, 2018 at 07:14 AM
+-- Host: localhost
+-- Generation Time: Dec 15, 2018 at 04:24 AM
 -- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- PHP Version: 5.6.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `date` datetime NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `username`, `date`, `message`) VALUES
+(3, 'Michael', '2018-12-15 09:00:00', 'Does anyone have any tips for singing lessons?');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `createlist`
 --
 
@@ -40,8 +60,9 @@ CREATE TABLE `createlist` (
 --
 
 INSERT INTO `createlist` (`userID`, `listTitle`, `listDescription`, `Visibility`) VALUES
-(3, 'Sing', '', 'pub'),
-(16, 'Fun', '', 'pub');
+(29, 'Concert Prep', '', 'pub'),
+(30, 'Chores', '', 'pub'),
+(30, 'Schoolwork', '', 'pub');
 
 -- --------------------------------------------------------
 
@@ -66,8 +87,36 @@ CREATE TABLE `createtask` (
 --
 
 INSERT INTO `createtask` (`userID`, `taskID`, `username`, `taskTitle`, `taskDescription`, `listTitle`, `Visibility`, `dueDate`, `priorityLVL`) VALUES
-(3, 17, '', 'Practice A', 'do this ', 'Sing ', 'pub', '2018-12-20', 'm'),
-(16, 18, '', 'Fun things AA', 'somethin', 'Fun ', 'pub', '0000-00-00', 'm');
+(29, 48, '', 'Dance Show', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2018-12-30', 'h'),
+(29, 49, '', 'Dance Practice', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2018-12-21', 'm'),
+(29, 50, '', 'Singing Exam Practice', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2018-12-29', 'l'),
+(29, 51, '', 'Dance Show B', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2019-01-21', 'm'),
+(29, 52, '', 'Vocal Lessons with Opera ', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2019-02-12', 'h'),
+(29, 53, '', 'Start Tour', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2019-02-03', 'h'),
+(29, 54, '', 'Call Mom', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2019-01-23', 'l'),
+(29, 55, '', 'Sell tickets', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2019-01-01', 'm'),
+(29, 56, '', 'Sell backstage passes', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2019-03-02', 'm'),
+(29, 57, '', 'Buy costumes', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Concert Prep ', 'pub', '2019-01-07', 'h'),
+(30, 58, '', 'Clean bathroom', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2018-12-25', 'h'),
+(30, 59, '', 'Clean room ', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2018-12-27', 'm'),
+(30, 60, '', 'Clean kitchen', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2019-01-01', 'l'),
+(30, 61, '', 'Organize bookshelf', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pri', '2019-12-29', 'h'),
+(30, 62, '', 'Organize work station ', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2019-01-04', 'h'),
+(30, 63, '', 'Study for 307', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2018-12-28', 'h'),
+(30, 64, '', 'Take out trash', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2019-01-03', 'l'),
+(30, 65, '', 'Call dentist', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2018-12-27', 'm'),
+(30, 66, '', 'Walk dog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2019-02-12', 'h'),
+(30, 67, '', 'Pick up cheque', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2019-01-10', 'h'),
+(30, 68, '', 'Drop off clothes', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2019-01-08', 'm'),
+(30, 69, '', 'Study for Econ227', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Schoolwork ', 'pub', '2019-01-21', 'h'),
+(30, 70, '', 'Study for Math223', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Schoolwork ', 'pub', '2019-02-12', 'h'),
+(30, 71, '', 'Review Phys 180 notes', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Schoolwork ', 'pub', '2019-12-12', 'l'),
+(30, 72, '', 'Math 223 Review Session', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Schoolwork ', 'pub', '2019-03-12', 'm'),
+(30, 73, '', 'Redo midterms', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Schoolwork ', 'pub', '2019-12-29', 'l'),
+(30, 74, '', 'Redo old assignments', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Schoolwork ', 'pub', '2019-10-17', 'm'),
+(30, 75, '', 'Office hourse for 273', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Schoolwork ', 'pub', '2018-12-28', 'h'),
+(30, 76, '', 'Go for run', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nibh ultrices, bibendum metus nec, tristique erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Chores ', 'pub', '2018-12-24', 'l'),
+(30, 77, '', 'Buy tea', 'oinsoivnaeorlnves', 'Chores ', 'pub', '2018-12-29', 'l');
 
 -- --------------------------------------------------------
 
@@ -89,12 +138,40 @@ CREATE TABLE `register` (
 --
 
 INSERT INTO `register` (`id`, `username`, `password`, `gender`, `email`, `phone`) VALUES
-(16, 'Cardi', '9d5ed678fe57bcca610140957afab571', 'f', 'abcde', 1234567899),
-(17, 'Jack', '4ff9fc6e4e5d5f590c4f2134a8cc96d1', 'm', 'jack', 55656454545465);
+(28, 'Bruno', '38ba178e70015dc411a7337bdcd834ed', '', 'sohcowne', 0),
+(29, 'Michael', '7930c951e609e461e8226004ed91a985', '', 'ncionevcwevc', 0),
+(30, 'Britney', '9ae7efb0ebbf43a97ef3e7062d780237', '', 'novnoewvoiwenap', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reply`
+--
+
+CREATE TABLE `reply` (
+  `rid` int(11) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `comment_Id` int(11) NOT NULL,
+  `time_updated` datetime NOT NULL,
+  `replied_message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `reply`
+--
+
+INSERT INTO `reply` (`rid`, `username`, `comment_Id`, `time_updated`, `replied_message`) VALUES
+(3, 'Britney', 3, '2018-12-15 09:17:00', 'Yea come to the studio this weekend Michael!');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `createtask`
@@ -110,20 +187,38 @@ ALTER TABLE `register`
   ADD UNIQUE KEY `username` (`username`);
 
 --
+-- Indexes for table `reply`
+--
+ALTER TABLE `reply`
+  ADD PRIMARY KEY (`rid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `createtask`
 --
 ALTER TABLE `createtask`
-  MODIFY `taskID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `taskID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `reply`
+--
+ALTER TABLE `reply`
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
